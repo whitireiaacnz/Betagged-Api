34a76a0ada1bb0d58cd997d85ac674822aa41040
whitireiaacnz <ashleyrodrigues93@gmail.com>
2024-11-15 17:05:18 +0530

    Aws file

---
+++ BeTagged/BeTagged.CoreAPIs/BeTagged.Common/Configurations/AwsConfiguration.cs
@@ -0,0 +1,18 @@
+﻿namespace BeTagged.Common.Configurations;
+
+public class AwsConfiguration
+{
+    public const string Section = "AwsConfig";
+
+    public string AccessKeyId { get; set; }
+
+    public string AccessKeySecret { get; set; }
+
+    public string S3BucketName { get; set; }
+
+    public Uri S3CloudFrontDomain { get; set; }
+
+    public string S3CloudFrontUrlSignerKeyPairId { get; set; }
+
+    public string S3CloudFrontUrlSignerPrivateKey { get; set; }
+}
